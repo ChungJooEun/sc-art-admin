99d3496f9d36f50666afc75164fc3c3e9b4623f2
ChungJooEun <nina2439@naver.com>
2021-11-08 20:25:58 +0900

    useEffect script 추가 코드

--- src/components/main-design/SkinAndBannerDesignView.js
+++ src/components/main-design/SkinAndBannerDesignView.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import GlobalBar from "../basic-components/GlobalBar";
 import PageTitle from "../basic-components/PageTitle";
@@ -20,6 +20,46 @@ const pagePathList = [
 ];
 
 const SkinAndBannerDesignView = () => {
+  useEffect(() => {
+    const srcList = [
+      "/assets/vendor/jquery.min.js",
+      "/assets/vendor/popper.min.js",
+      "/assets/vendor/bootstrap.min.js",
+      "/assets/vendor/perfect-scrollbar.min.js",
+      "/assets/vendor/dom-factory.js",
+      "/assets/vendor/material-design-kit.js",
+      "/assets/js/app.js",
+      "/assets/js/hljs.js",
+      "/assets/js/settings.js",
+      "/assets/vendor/moment.min.js",
+      "/assets/vendor/moment-range.js",
+      "/assets/vendor/Chart.min.js",
+      "/assets/js/chartjs.js",
+      "/assets/js/chartjs-rounded-bar.js",
+      "/assets/js/page.projects.js",
+      "/assets/js/page.analytics-2-dashboard.js",
+      "/assets/vendor/list.min.js",
+      "/assets/js/list.js",
+      "/assets/js/toggle-check-all.js",
+      "/assets/js/check-selected-row.js",
+      "/assets/js/app-settings.js",
+    ];
+    let scriptList = [];
+
+    for (let i = 0; i < srcList.length; i++) {
+      const script = document.createElement("script");
+      script.src = process.env.PUBLIC_URL + srcList[i];
+      scriptList.push(script);
+      document.body.appendChild(script);
+    }
+
+    return () => {
+      for (let i = 0; i < scriptList.length; i++) {
+        document.body.removeChild(scriptList[i]);
+      }
+    };
+  });
+
   return (
     <>
       <div className="preloader">
