11f81fc9f66bddc012f3f2bc1b2d1762563150d7
ChungJooEun <nina2439@naver.com>
2021-11-08 14:51:25 +0900

    페이지 타이틀 컴포넌트

---
+++ src/components/basic-components/PageTitle.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const PageTitle = ({ pageTitle, pagePathList }) => {
+  return (
+    <div className="border-bottom-2 py-32pt position-relative z-1">
+      <div className="container-fluid page__container d-flex flex-column flex-md-row align-items-center text-center text-sm-left">
+        <div className="flex d-flex flex-column flex-sm-row align-items-center mb-24pt mb-md-0">
+          <div className="mb-24pt mb-sm-0 mr-sm-24pt">
+            <h2 className="mb-0">{pageTitle}</h2>
+            <ol className="breadcrumb p-0 m-0">
+              {pagePathList.map((pathListItem) => (
+                <li className="breadcrumb-item">
+                  <Link to={pathListItem.pageUrl}>{pathListItem.pageName}</Link>
+                </li>
+              ))}
+              <li className="breadcrumb-item active">{pageTitle}</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PageTitle;
