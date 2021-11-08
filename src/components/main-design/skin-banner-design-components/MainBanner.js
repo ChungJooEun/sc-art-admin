8782e9dd06a969bbf0156b9af8ae272c0e36f3eb
ChungJooEun <nina2439@naver.com>
2021-11-08 17:31:20 +0900

    메인 디자인 관리 > 메인 배너/스킨 관리 페이지 - 메인 배경 항목 컴포넌트

---
+++ src/components/main-design/skin-banner-design-components/MainBanner.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+const MainBanner = () => {
+  return (
+    <div className="page-section">
+      <div className="page-separator">
+        <div className="page-separator__text">메인배경</div>
+      </div>
+      <div className="list-group">
+        <div className="list-group-item">
+          <div
+            role="group"
+            aria-labelledby="label-question"
+            className="m-0 form-group"
+          >
+            <div className="form-row">
+              <div className="flex" style={{ maxWidth: "100%" }}>
+                <img
+                  src="../assets/images/256_rsz_thomas-russell-751613-unsplash.jpg"
+                  className="avatar-img rounded"
+                  alt=""
+                  data-dz-thumbnail
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="list-group-item">
+          <div
+            role="group"
+            aria-labelledby="label-question"
+            className="m-0 form-group"
+          >
+            <div className="form-row align-items-center">
+              <label
+                id="label-question"
+                for="question"
+                className="col-md-2 col-form-label form-label"
+              >
+                이미지
+              </label>
+              <div className="col-md-10">
+                <input
+                  type="file"
+                  className=""
+                  id="customFileUploadMultiple"
+                  multiple
+                />
+                <label className="" for="customFileUploadMultiple"></label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainBanner;
