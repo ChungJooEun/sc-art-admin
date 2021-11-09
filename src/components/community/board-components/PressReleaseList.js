89f6664950fdeec09abf6b68e0339401757e5123
ChungJooEun <nina2439@naver.com>
2021-11-09 14:06:09 +0900

    커뮤니티 > 보도자료 페이지 - 보도자료 리스트 테이블 컴포넌트

---
+++ src/components/community/board-components/PressReleaseList.js
@@ -0,0 +1,63 @@
+import React from "react";
+import PressReleaseListItem from "./PressReleaseListItem";
+
+const PressReleaseList = () => {
+  return (
+    <table className="table mb-0 thead-border-top-0 table-nowrap">
+      <thead>
+        <tr>
+          <th style={{ width: "12px" }} className="pr-0">
+            <div className="custom-control custom-checkbox">
+              <input
+                type="checkbox"
+                className="custom-control-input js-toggle-check-all"
+                data-target="#staff"
+                id="customCheckAllstaff"
+              />
+              <label className="custom-control-label" for="customCheckAllstaff">
+                <span className="text-hide">Toggle all</span>
+              </label>
+            </div>
+          </th>
+          <th style={{ width: "12px" }}>no.</th>
+          <th style={{ width: "150px" }}>
+            <a
+              href="javascript:void(0)"
+              className="sort"
+              data-sort="js-lists-values-cultural-seocho-festival-name"
+            >
+              제목
+            </a>
+          </th>
+          <th style={{ width: "64px" }}>
+            <a
+              href="javascript:void(0)"
+              className="sort"
+              data-sort="js-lists-values-public"
+            >
+              조회수
+            </a>
+          </th>
+          <th style={{ width: "64px" }}>
+            <a
+              href="javascript:void(0)"
+              className="sort"
+              data-sort="js-lists-values-public"
+            >
+              등록일
+            </a>
+          </th>
+        </tr>
+      </thead>
+      <tbody className="list" id="staff">
+        <PressReleaseListItem />
+        <PressReleaseListItem />
+        <PressReleaseListItem />
+        <PressReleaseListItem />
+        <PressReleaseListItem />
+      </tbody>
+    </table>
+  );
+};
+
+export default PressReleaseList;
---
+++ src/components/community/board-components/PressReleaseListItem.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const PressReleaseListItem = () => {
+  return (
+    <tr className="selected">
+      <td className="pr-0">
+        <div className="custom-control custom-checkbox">
+          <input
+            type="checkbox"
+            className="custom-control-input js-check-selected-row"
+            checked=""
+            id="customCheck1_1"
+          />
+          <label className="custom-control-label" for="customCheck1_1">
+            <span className="text-hide">Check</span>
+          </label>
+        </div>
+      </td>
+      <td className="js-lists-values-place small">9</td>
+      <td className="text-aline-left">
+        <div
+          className="media flex-nowrap align-items-center"
+          style={{ whiteSpace: "nowrap" }}
+        >
+          <div className="media-body">
+            <div className="d-flex flex-column">
+              <p className="mb-0 txt_line_table_title">
+                <a href="">
+                  <strong className="js-lists-values-cultural-seocho-festival-name">
+                    제목
+                  </strong>
+                </a>
+              </p>
+              <small className="js-lists-values-employee-email text-50"></small>
+            </div>
+          </div>
+        </div>
+      </td>
+      <td className="js-lists-values-status small">125</td>
+      <td className="js-lists-values-public small">21.07.30</td>
+    </tr>
+  );
+};
+
+export default PressReleaseListItem;
