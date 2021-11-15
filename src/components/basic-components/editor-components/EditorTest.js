73cd6b0b424d4913133f1aa239a00996b0042079
ChungJooEun <nina2439@naver.com>
2021-11-15 13:02:11 +0900

    문화공간 관리 > 서리풀 청년 아트 센터/갤러리 - 상세조회 기능 - 등록 기능

--- src/components/basic-components/editor-components/EditorTest.js
+++ src/components/basic-components/editor-components/EditorTest.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import ReactQuill from "react-quill";
 import CustomToolbar from "./CustomToolbar";
--- src/components/place/ScPlaceView.js
+++ src/components/place/ScPlaceView.js
@@ -1,4 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+
 import EditorTest from "../basic-components/editor-components/EditorTest";
 import GlobalBar from "../basic-components/GlobalBar";
 import PageTitle from "../basic-components/PageTitle";
@@ -17,15 +20,55 @@ const pagePathList = [
 ];
 const options = [
   {
-    value: 0,
+    value: "TEMP_SAVE",
     name: "임시저장",
   },
   {
-    value: 1,
+    value: "POST",
     name: "게시",
   },
 ];
 const ScPlaceView = ({ pageTitle }) => {
+  const history = useHistory();
+
+  const [name, setName] = useState(null);
+  const [postState, setPostState] = useState(null);
+  const [detail, setDetail] = useState(null);
+  const getDetail = (e) => {
+    setDetail(e);
+  };
+
+  const postScPlace = async (placeData) => {
+    const url = "/api/admin/seoripul-space/regist";
+
+    try {
+      const response = await axios.post(url, placeData, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      });
+
+      console.log(response.status);
+      if (response.status === 200) {
+        console.log(response.data);
+        history.push("/place/place-manage");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const onSubmitEvent = (formState) => {
+    let formData = new FormData();
+
+    formData.append("name", name);
+    formData.append("state", formState);
+    formData.append("more_information", detail);
+    formData.append("userid", window.sessionStorage.getItem("userid"));
+
+    postScPlace(formData);
+  };
+
   useEffect(() => {
     const srcList = [
       `${process.env.PUBLIC_URL}/assets/vendor/jquery.min.js`,
@@ -59,12 +102,41 @@ const ScPlaceView = ({ pageTitle }) => {
       document.body.appendChild(script);
     }
 
+    const getScplaceInfo = async () => {
+      const url = "/api/admin/seoripul-space/list";
+
+      const res = await axios.get(url);
+
+      if (res.status === 200) {
+        let findData = false;
+
+        for (let i = 0; i < res.data.list.length; i++) {
+          if (res.data.list[i].name === pageTitle) {
+            setName(res.data.list[i].name);
+            setPostState(res.data.list[i].state);
+            setDetail(res.data.list[i].more_information);
+            findData = true;
+            break;
+          }
+        }
+
+        if (!findData) {
+          setName(pageTitle);
+          setPostState("");
+          setDetail("");
+        }
+      }
+    };
+
+    getScplaceInfo();
+
     return () => {
       for (let i = 0; i < scriptList.length; i++) {
         document.body.removeChild(scriptList[i]);
       }
     };
-  });
+  }, [pageTitle]);
+
   return (
     <div
       className="mdk-drawer-layout js-mdk-drawer-layout"
@@ -82,12 +154,16 @@ const ScPlaceView = ({ pageTitle }) => {
                 <div className="page-separator__text">상세정보</div>
               </div>
 
-              <EditorTest />
+              <EditorTest more_information={detail} getDetail={getDetail} />
             </div>
             <div className="page-section">
               <div className="detail_under_menu ">
                 <div className="card">
-                  <PostSaveBtn options={options} />
+                  <PostSaveBtn
+                    options={options}
+                    onSubmitEvent={onSubmitEvent}
+                    state={postState}
+                  />
                 </div>
               </div>
             </div>
