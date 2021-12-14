270d23ad75889c7ff51cc16b806001cd5815314e
ChungJooEun <nina2439@naver.com>
2021-12-14 11:11:08 +0900

    setUpProxy 업데이트

--- src/setUpProxy.js
+++ src/setUpProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("https-proxy-middleware");
 module.exports = function (app) {
   // 관리자 목록 보기
   app.use(
     "/api/admin/list",
     createProxyMiddleware({
-      target: "http://118.67.154.134:9000",
+      target: "https://culture.seocho.go.kr:8443/admin-service",
       changeOrigin: true,
     })
   );
@@ -13,7 +13,7 @@ module.exports = function (app) {
   app.use(
     "/api/admin/detail",
     createProxyMiddleware({
-      target: "http://118.67.154.134:9000",
+      target: "https://culture.seocho.go.kr:8443/admin-service",
       changeOrigin: true,
     })
   );
@@ -22,7 +22,7 @@ module.exports = function (app) {
   app.use(
     "/api/admin/user/list",
     createProxyMiddleware({
-      target: "http://118.67.154.134:9000",
+      target: "https://culture.seocho.go.kr:8443/admin-service",
       changeOrigin: true,
     })
   );
@@ -31,7 +31,7 @@ module.exports = function (app) {
   app.use(
     "/api/admin/user/detail",
     createProxyMiddleware({
-      target: "http://118.67.154.134:9000",
+      target: "https://culture.seocho.go.kr:8443/admin-service",
       changeOrigin: true,
     })
   );
@@ -39,16 +39,16 @@ module.exports = function (app) {
   app.use(
     "/api/admin/regist",
     createProxyMiddleware({
-      target: "http://118.67.154.134:9000",
+      target: "https://culture.seocho.go.kr:8443/admin-service",
       changeOrigin: true,
     })
   );
 
   // 메인 디자인 관련
   app.use(
-    "/api/main",
+    "/api/main/",
     createProxyMiddleware({
-      target: "http://118.67.154.134:22000",
+      target: "https://culture.seocho.go.kr:8443/community-service",
       changeOrigin: true,
     })
   );
@@ -57,7 +57,7 @@ module.exports = function (app) {
   app.use(
     "/api/main/theme",
     createProxyMiddleware({
-      target: "http://118.67.154.134:22000",
+      target: "https://culture.seocho.go.kr:8443/community-service",
       changeOrigin: true,
     })
   );
@@ -66,7 +66,7 @@ module.exports = function (app) {
   app.use(
     "/api/admin/main/",
     createProxyMiddleware({
-      target: "http://118.67.154.134:3000",
+      target: "https://culture.seocho.go.kr:3000",
       changeOrigin: true,
     })
   );
@@ -74,11 +74,10 @@ module.exports = function (app) {
   app.use(
     "/api/admin",
     createProxyMiddleware({
-      target: "http://118.67.154.118:3000",
-      // target: "http://localhost:3000",
+      target: "https://culture.seocho.go.kr:3000",
       changeOrigin: true,
       router: {
-        "/api/admin": "http://118.67.154.118:3000",
+        "/api/admin": "https://culture.seocho.go.kr:3000",
       },
     })
   );
@@ -86,7 +85,7 @@ module.exports = function (app) {
   app.use(
     "/login",
     createProxyMiddleware({
-      target: "http://118.67.154.134:9000",
+      target: "https://culture.seocho.go.kr:8443/admin-service",
       changeOrigin: true,
     })
   );
